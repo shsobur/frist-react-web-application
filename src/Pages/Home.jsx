3bd4e93144c55ce2bac136663d10313ef0775629
shsobur <shsoburhossen951@gmail.com>
2024-03-25 02:28:49 +0600

    home page complited

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import weve from "../assets/wave.svg";
 
 const Home = () => {
   return (
-    <div className="min-h-[calc(100vh-116px)] flex flex-col items-center justify-center">
+    <div className="min-h-[calc(100vh-116px)] flex flex-col items-center justify-center relative  ">
       <Hero></Hero>
+      <img className="absolute bottom-0 w-full" src={weve} />
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,56 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <div>
-      <div className="hero bg-base-200">
+      <div className="hero -mt-16">
         <div className="hero-content text-center">
           <div className="max-w-3xl">
-            <h1 className="text-4xl font-bold">Welcome to LogicNactor</h1>
+            <h1 className="text-5xl font-bold">
+              Welcome to{" "}
+              <span className="bg-gradient-to-r from-primary via-blue-500 to-secondary text-transparent bg-clip-text bg-300% animate-gradient">
+                LogicNactor
+              </span>
+            </h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary mx-8">Read Blogs</button>
-            <button className="btn btn-primary">Bookmarks</button>
+            <div className="flex items-center justify-center  gap-8">
+              <div>
+                <Link
+                  to="/blogs"
+                  className="relative inline-block text-lg group"
+                >
+                  <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-black rounded-lg group-hover:text-white">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-primary group-hover:-rotate-180 ease"></span>
+                    <span className="relative">Read Blogs</span>
+                  </span>
+                  <span
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    data-rounded="rounded-lg"
+                  ></span>
+                </Link>
+              </div>
+              <div>
+                <Link
+                  to="/bookmarks"
+                  className="relative inline-block text-lg group"
+                >
+                  <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-black rounded-lg group-hover:text-white">
+                    <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                    <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-primary group-hover:-rotate-180 ease"></span>
+                    <span className="relative">Boockmarks</span>
+                  </span>
+                  <span
+                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                    data-rounded="rounded-lg"
+                  ></span>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
