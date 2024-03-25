60a33b85bf478e917c07aea492e29307c4405d6d
shsobur <shsoburhossen951@gmail.com>
2024-03-25 15:41:46 +0600

    blogs setup finished

--- src/main.jsx
+++ src/main.jsx
@@ -13,19 +13,20 @@ const router = createBrowserRouter([
     element: <MainLayout></MainLayout>,
     children: [
       {
-        path: '/',
-        element: <Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
         path: "/blogs",
         element: <Blogs></Blogs>,
+        loader: () => fetch("https://dev.to/api/articles?per_page=20&top=7"),
       },
       {
         path: "/bookmarks",
         element: <Bookmarks></Bookmarks>,
       },
-    ]
-  }
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
